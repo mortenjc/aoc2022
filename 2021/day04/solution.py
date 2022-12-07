47f0dbab4ce01d5894bcc20254c37a865532c7f1
Morten Jagd Christensen <mortenjc@jcaps.com>
2022-12-07 17:01:53 +0100

    updates

---
+++ 2021/day04/solution.py
@@ -0,0 +1,30 @@
+#!/usr/local/bin/python3
+
+import argparse
+import sys
+#from collections import deque
+#from collections import defaultdict
+import copy
+
+
+def solve(lines):
+    pass
+
+def solve2(lines):
+    for line in lines:
+        pass
+
+
+
+if __name__ == '__main__':
+    ifile = sys.argv[1] if len(sys.argv) > 1 else 'test.txt'
+    print("<<{}>>".format(ifile))
+
+    #lines = [line.rstrip() for line in open(ifile, 'r')]
+    with open(ifile) as fin:
+        lines = (("\n" + fin.read().strip()).split('\n\n'))
+    print("############## A ##############")
+    solve(lines)
+    print("############## B ##############")
+    solve2(lines)
+    print("###############################")
