b4960703ba0d4a8d59de0f069bf5a89feb8bcbd0
Morten Jagd Christensen <mortenjc@jcaps.com>
2022-12-13 15:27:31 +0100

    refactor after videos

--- 2022/day13/solution.py
+++ 2022/day13/solution.py
@@ -2,51 +2,40 @@
 
 import sys
 #import defaultdict
-from copy import deepcopy
+#from copy import deepcopy
+import functools
 #from collections import deque
 
 
 def compare(l, r):
-    #print(f'compare(): l {l}, r{r}')
-    if type(l) is list and type(r) is list:
-        done = False
-        while (not done):
-            if len(l) and len(r): # both have data
-                a = l.pop(0)
-                b = r.pop(0)
-                compare(a, b)
-            elif len(l) and not len(r):
-                raise Exception("bad order")
-            elif not(len(l)) and len(r):
-                raise Exception("good order")
-            else:
-                done = True
-
-    elif type(l) is int and type(r) is int:
+    if type(l) is int and type(r) is int:
         if l == r:
-            return
+            return 0
         if l < r:
-            raise Exception("good order")
+            return -1
         else:
-            raise Exception("bad order")
+            return 1
 
+    if type(l) is list and type(r) is list:
+        i = 0
+        while i < len(l) and i < len(r):
+            c = compare(l[i], r[i])
+            if c == -1:
+                return -1
+            if c == 1:
+                return 1
+            i += 1
+        if i == len(l) and i < len(r):
+            return -1
+        elif i < len(l) and i == len(r):
+            return 1
+        else:
+            return 0
     elif type(l) is list and type(r) is int:
-        compare(l, [r])
+        return compare(l, [r])
     elif type(l) is int and type(r) is list:
-        compare([l], r)
-    else:
-        assert False
+        return compare([l], r)
 
-def cmp(a, b):
-    try:
-        #print(f'compare {l} with {r}')
-        compare(a, b)
-    except Exception as inst:
-        #print(inst.args)
-        if inst.args[0] == 'good order':
-            return True
-        else:
-            return False
 
 infile = sys.argv[1] if len(sys.argv) > 1 else 'test.txt'
 print("<<{}>>".format(infile))
@@ -57,15 +46,13 @@ with open(infile) as fin:
         lines = ((fin.read().strip()).split('\n\n'))
 
 # part 1
-n = 0
+S1 = 0
 for i, line in enumerate(lines):
     pair = i + 1
     l, r = line.replace('\n', ' ').split()
-    l = eval(l)
-    r = eval(r)
-    if cmp(l, r):
-        n += pair
-S1 = n
+    if compare(eval(l), eval(r)) == -1:
+        S1 += pair
+
 
 #part 2
 all = []
@@ -76,25 +63,12 @@ for line in lines:
 all.append([[2]])
 all.append([[6]])
 
-for i in range(len(all)):
-    for j in range(len(all)):
-        if i == j:
-            continue
-        a = deepcopy(all[i])
-        b = deepcopy(all[j])
-        if cmp(a, b):
-            tmp = all[i]
-            all[i] = all[j]
-            all[j] = tmp
-da = 0
-db = 0
-for i, val in enumerate(all):
-    if val == [[2]]:
-        da = i + 1
-    if val == [[6]]:
-        db = i + 1
-S2 = da*db
+sorted(all, key=functools.cmp_to_key(compare))
 
+S2 = 1
+for i, val in enumerate(all):
+    if val == [[2]] or val == [[6]]:
+        S2 = S2 * (i + 1)
 
 print("------------- A -------------")
 print(f'S1 {S1}')
