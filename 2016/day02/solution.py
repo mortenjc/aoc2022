acfd7a87e3f180b3d9048c29be4f4d4b9ef3b682
Morten Jagd Christensen <mortenjc@jcaps.com>
2023-12-22 22:51:08 +0100

    refactor

--- 2016/day02/solution.py
+++ 2016/day02/solution.py
@@ -11,19 +11,13 @@ S2 = 0
 with open(infile) as fin:
     lines = ((fin.read().strip()).split('\n'))
 
-
 key1 = '123\n456\n789'
-key2 = '..1..\n.234.\n56789\n.ABC.\n..D..'
-
-F = [[x for x in l] for l in key1.split('\n')]
-G = [[x for x in l] for l in key2.split('\n')]
-
+key2 = '  1  \n 234 \n56789\n ABC \n  D  '
 
 dir = {'U':(-1,0), 'D':(1,0), 'L':(0,-1), 'R':(0,1)}
 
-
-
-def solve(M):
+def solve(key):
+    M = [[x for x in l] for l in key.split('\n')]
     ans = ''
     R = len(M)
     C = len(M[0])
@@ -34,22 +28,21 @@ def solve(M):
             if M[r][c] == '5':
                 rr = r
                 cc = c
-    print(rr,cc)
 
     for l in lines:
         for ch in l:
             dr, dc = dir[ch]
             nr = rr + dr
             nc = cc + dc
-            if 0<=nr<R and 0<=nc<C and M[nr][nc] != '.':
+            if 0<=nr<R and 0<=nc<C and M[nr][nc] != ' ':
                 rr = nr
                 cc = nc
         ans = ans + M[rr][cc]
     return ans
 
 
-S1 = solve(F)
-S2 = solve(G)
+S1 = solve(key1)
+S2 = solve(key2)
 
 
 print("------------- A -------------")
